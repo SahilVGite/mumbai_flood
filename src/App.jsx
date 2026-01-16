5bc3383026cc3ed8bc6060a988388bd3fcae8475
Sahil Gite <sahilvgite@gmail.com>
2026-01-17 02:15:06 +0530

    some extra coordinates added for better visualisation

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,131 @@ function App() {
         properties: { level: "high" },
         geometry: { type: "Point", coordinates: [72.831754, 19.050541] },
       },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.811517, 19.140071] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.828068, 19.151831] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "low" },
+        geometry: { type: "Point", coordinates: [72.824531, 19.121093] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.866120, 19.100776] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.824107, 19.111202] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.840233, 19.122964] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "low" },
+        geometry: { type: "Point", coordinates: [72.866828, 19.097033] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.820570, 19.045025] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "low" },
+        geometry: { type: "Point", coordinates: [72.824248, 19.062407] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.847634, 19.047569] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.876359, 19.126181] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.863550, 19.114783] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.873375, 19.106205] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.877230, 19.138283] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.870017, 19.146624] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.839397, 19.135664] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.794736, 19.151030] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "low" },
+        geometry: { type: "Point", coordinates: [72.831541, 19.157407] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "low" },
+        geometry: { type: "Point", coordinates: [72.841506, 19.152549] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.791200, 19.132203] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.788789, 19.143591] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.802129, 19.145868] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "moderate" },
+        geometry: { type: "Point", coordinates: [72.794736, 19.150423] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.784771, 19.165757] },
+      },
+      {
+        type: "Feature",
+        properties: { level: "high" },
+        geometry: { type: "Point", coordinates: [72.832988, 19.160443] },
+      },
     ],
   };
 
